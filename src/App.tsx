b8285cd33db8535627e0b3207b7a0ca14b92fedf
YOU <khy960101@gmail.com>
2023-06-20 23:54:47 +0900

    허스키 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  var variable;
+  var variable = '4';
   return (
     <div className="App">
       <header className="App-header">
