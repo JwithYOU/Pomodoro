546b2150c9abeb3a598d2e35fd26b58da1fec3e0
YOU <khy960101@gmail.com>
2023-06-21 19:30:43 +0900

    fix: react-router-dom 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SetForm from './pages/SetForm';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SetForm />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <SetForm />;
 }
 
 export default App;
